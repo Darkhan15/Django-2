f807ab8146852656dd686cafc9a75911ffdd48b4
Darkhan15 <darkhan.tleubayev@mail.ru>
2024-04-08 02:28:24 +0500

    Add files via upload
    done

---
+++ news/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import News, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 5
+
+class AdminNews(admin.ModelAdmin):
+    inlines = [CommentInline]
+    list_display = ["title", "content", "created_at", "has_comments"]
+    list_filter = ["created_at"]
+
+
+admin.site.register(News, AdminNews)
---
+++ news/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import News, Comment
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2"
+        ]
